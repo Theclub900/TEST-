28e66a7325114c1289bddf533997df0ab49b111c
Theclub900 <theclub900@icloud.com>
2025-12-04 14:50:50 -0800

    Add files via upload

---
+++ NOVA/non profit revised /NOVA/script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+   <html>
+   <head>
+	   <link rel="stylesheet" href="styles.css">
+   </head>
+   <body>
+	   <!-- Your content -->
+	   
+	   <script src="script.js"></script>
+   </body>
+   </html>
